3f2dff3d2075fa313763baefa1a05a547b58caf3
bossclement <bosstclement@gmail.com>
2024-11-08 08:39:14 +0200

    array statistics

---
+++ arrayStatistics.js
@@ -0,0 +1,9 @@
+const arrayStats = function(arr) {
+    const obj = {
+        sum: arr.reduce(function(sum, num) { return sum + num }, 0),
+        min: Math.min(...arr),
+        max: Math.max(...arr)
+    }
+    return obj;
+}
+console.log(arrayStats([1, 2, 3, 4, 5])); // output { sum: 15, min: 1, max: 5 }
